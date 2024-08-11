b859982231c4020ca8a98b8f498d55d3cacf6551
Sujit  Kumar <162633274+sujit5020@users.noreply.github.com>
2024-08-11 23:52:57 +0530

    Time: 5 ms (50.42%), Space: 8.9 MB (64.09%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.length() - 1;
+        
+        while (left < right) {
+            // Move left pointer to the next alphanumeric character
+            while (left < right && !isalnum(s[left])) {
+                left++;
+            }
+            // Move right pointer to the previous alphanumeric character
+            while (left < right && !isalnum(s[right])) {
+                right--;
+            }
+            // Compare the characters
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+};
