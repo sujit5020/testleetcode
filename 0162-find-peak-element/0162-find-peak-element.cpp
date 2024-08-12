5548f270fb0b9f14842a6b0b8c77c1d45d96d65f
Sujit  Kumar <162633274+sujit5020@users.noreply.github.com>
2024-08-12 23:17:28 +0530

    Time: 0 ms (100%), Space: 11.5 MB (35%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -8,16 +8,14 @@ public:
             int mid = left + (right - left) / 2;
 
             if (nums[mid] > nums[mid + 1]) {
-                // If the mid element is greater than the next element, 
-                // then the peak must be in the left half or could be mid itself
+            
                 right = mid;
             } else {
-                // If the mid element is less than the next element,
-                // then the peak must be in the right half
+            
                 left = mid + 1;
             }
         }
-        // At the end of the loop, left == right, which should point to the peak element
+
         return left;
     }
 };
